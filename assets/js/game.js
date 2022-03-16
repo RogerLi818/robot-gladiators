3b9215181168c279cd98b9aa864d1e044cb7466e
RogerLi818 <jackliteng@yahoo.com>
2022-03-15 23:36:12 -0700

    adding basic function to javascript

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,16 @@
-window.alert("this is an alart! JavaScript is runing!");
+var playerName = window.prompt("What's your robot's name?")
+
+// Note the lack of quotation marks around playerName
+console.log(playerName);
+
+console.log(10+15);
+console.log('this is a dog named ' + playerName)
+
+// this creats a function named "fight"
+
+function fight() {
+    window.alert("the fight has begun!");
+}
+
+// fight();
+
