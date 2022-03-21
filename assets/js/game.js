702bc95a3d4eb36a62349688ffcabff06ad3d351
RogerLi818 <jackliteng@yahoo.com>
2022-03-20 21:36:53 -0700

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,14 +5,14 @@ var playerHealth = 100;
 var playerAttack = 10;
 
 // you can also log multiple values at one lik this
-console.log(playerName, playerHealth, playerAttack);
+// console.log(playerName, playerHealth, playerAttack);
 
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
 //function expression
-var fight = function() {
+var fight = function(enemyName) {
     //Alert players that they are starting the round
     window.alert("Welcome to Robot Galadiators!");
 
@@ -27,22 +27,22 @@ var fight = function() {
         enemyHealth = enemyHealth - playerAttack;
         // Log a resulting message to the console so we know that it worked.
         console.log(
-        playerName + " attacked " + enemyNames + ". " + enemyNames +" now has " + enemyHealth +" health remaining."
+        playerName + " attacked " + enemyName + ". " + enemyName +" now has " + enemyHealth +" health remaining."
         );
 
         //check enemy's health
         if (enemyHealth<=0) {
-        window.alert(enemyNames + " has died!");
+        window.alert(enemyName + " has died!");
         }
         else {
-        window.alert(enemyNames + " still has " + enemyHealth + " health left.");
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
         }
         // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
         playerHealth = playerHealth - enemyAttack;
 
         // Log a resulting message to the console so we know that it worked.
         console.log(
-            enemyNames + " attached " + playerName  + ". " + playerName + " now has " + playerHealth + " health remaining."
+            enemyName + " attached " + playerName  + ". " + playerName + " now has " + playerHealth + " health remaining."
         )
 
         //check player's health
@@ -65,11 +65,24 @@ var fight = function() {
         //if no (false), ask question again by running fight() again
         else{
             fight();
-        }
+            };
+            
+        
     } else{
         window.alert("You need to choose a valid option. Try again!")
     }
 };
+
+
+console.log(enemyNames);
+console.log(enemyNames.length);
+console.log(enemyNames[0]);
+console.log(enemyNames[1]);
+console.log(enemyNames[2]);
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+};
+
 //Game status
 //"win" - player robot has defeated all enemy-robot
 //  *Fight all enemy-robots
@@ -80,12 +93,11 @@ var fight = function() {
 //fight();
 
 
-
-for(var i=0; i<enemyNames.length; i++){
+/*for(var i=0; i<enemyNames.length; i++){
     console.log(enemyNames[i]);
     console.log(i);
-    console.log(enemyNames[i]+" is at " +i+" index");
-}
+    console.log(enemyNames[i]+" is at " +i+" index");*/
+
 
 /* for([initial expression]; [condition]; [increment expression]) {
    statement
